a0eacdf78c1731a9f7a7197d994e61fe02503678
Varloot_Estelle <estelle.varloot@ens-rennes.fr>
2016-12-01 17:16:33 +0100

    hoho

---
+++ brouillon_proj.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 16:34:39 2016
+
+@author: perfection
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import cmath
+from numpy.fft import fft
+from winsound import PlaySound
+import winsound
+from scipy.io.wavfile import read as wavread
+
