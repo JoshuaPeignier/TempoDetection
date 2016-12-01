b82f85002adb168c0633405094819799719dd9f4
Varloot_Estelle <estelle.varloot@ens-rennes.fr>
2016-12-01 17:27:14 +0100

    hotmew

--- brouillon_proj.py
+++ brouillon_proj.py
@@ -14,3 +14,14 @@ from winsound import PlaySound
 import winsound
 from scipy.io.wavfile import read as wavread
 
+#%% Mise en place de filtre passe bande
+
+H1 = 200
+H2 = 400
+H3 = 800
+H4 = 1600
+H5 = 3200
+
+def filtre_1:
+
+#%% Mise en place de filtre passe haut
